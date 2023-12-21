3a164ac866b6adf928aabb297f9459f47bade9a4
Mr.Eric <451008604@qq.com>
2023-12-22 01:25:27 +0800

    fix

--- web/src/view/dashboard/dashboardCharts/echartsLine.vue
+++ web/src/view/dashboard/dashboardCharts/echartsLine.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="dashboard-line-box">
-    <div class="dashboard-line-title">
-      访问趋势
-    </div>
+    <div class="dashboard-line-title"></div>
     <div ref="echart" class="dashboard-line" />
   </div>
 </template>
@@ -10,94 +8,91 @@
 <script setup>
 import * as echarts from 'echarts'
 import { nextTick, onMounted, onUnmounted, ref, shallowRef } from 'vue'
-// import 'echarts/theme/macarons'
-
-var dataAxis = []
-for (var i = 1; i < 13; i++) {
-  dataAxis.push(`${i}月`)
-}
-var data = [
-  220,
-  182,
-  191,
-  234,
-  290,
-  330,
-  310,
-  123,
-  442,
-  321,
-  90,
-  149,
-]
-var yMax = 500
-var dataShadow = []
-
-// eslint-disable-next-line no-redeclare
-for (var i = 0; i < data.length; i++) {
-  dataShadow.push(yMax)
-}
+import 'echarts/theme/macarons'
 
 const chart = shallowRef(null)
 const echart = ref(null)
-const initChart = () => {
-  chart.value = echarts.init(echart.value /* 'macarons' */)
-  setOptions()
+const oneMinute = 60 * 1000;
+let data = [];
+let now = new Date();
+let value = Math.random() * 1000;
+
+const randomData = () => {
+  now = new Date(+now + oneMinute);
+  value = value + Math.random() * 21 - 10;
+  return {
+    name: now.toString(),
+    value: [now.getDate() + "日" + now.getHours() + ":" + now.getMinutes(), Math.round(value)]
+  };
 }
-const setOptions = () => {
+const setOption = () => {
+  if (chart.value == null) {
+    clearInterval(interval)
+    return
+  }
   chart.value.setOption({
     grid: {
       left: '40',
       right: '20',
-      top: '40',
+      top: '20',
       bottom: '20',
     },
+    tooltip: {
+      trigger: 'axis',
+      formatter: function (params) {
+        params = params[0];
+        var date = new Date(params.name);
+        return (
+          date.getHours() + ':' + (date.getMinutes()) + '  ' + params.value[1]
+        );
+      },
+      axisPointer: {
+        animation: false
+      }
+    },
     xAxis: {
-      data: dataAxis,
-      axisTick: {
-        show: false,
-      },
-      axisLine: {
-        show: false,
-      },
-      z: 10,
+      type: 'category',
+      splitLine: {
+        show: false
+      }
     },
     yAxis: {
-      axisLine: {
-        show: false,
-      },
-      axisTick: {
-        show: false,
-      },
-      axisLabel: {
-        textStyle: {
-          color: '#999',
-        },
-      },
+      type: 'value',
+      boundaryGap: [0, '100%'],
+      splitLine: {
+        show: false
+      }
     },
     dataZoom: [
       {
-        type: 'inside',
-      },
+        type: "inside",
+      }
     ],
     series: [
       {
-        type: 'bar',
-        barWidth: '40%',
-        itemStyle: {
-          borderRadius: [5, 5, 0, 0],
-          color: '#188df0',
-        },
-        emphasis: {
-          itemStyle: {
-            color: '#188df0',
-          },
-        },
-        data: data,
-      },
-    ],
+        type: 'line',
+        showSymbol: false,
+        data: data
+      }
+    ]
   })
 }
+
+for (var i = 0; i < 24 * 60; i++) {
+  data.push(randomData());
+}
+
+const initChart = () => {
+  chart.value = echarts.init(echart.value, 'macarons')
+  setOption();
+}
+
+var interval = setInterval(function () {
+  data.shift();
+  data.push(randomData());
+
+  setOption();
+}, 60 * 1000);
 
 onMounted(async () => {
   await nextTick()
